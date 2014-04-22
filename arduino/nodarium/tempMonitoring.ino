5ef6542cfbba912381126500ccfa25d3f0f566ba
Motti Horesh <mottihoresh@gmail.com>
2014-04-22 18:41:50 -0400

    arduino/docs folders

---
+++ arduino/nodarium/tempMonitoring.ino
@@ -0,0 +1,19 @@
+
+// function to print a device's resolution
+void printResolution(DeviceAddress deviceAddress)
+{
+  Serial.print("Resolution: ");
+  Serial.print(sensors.getResolution(deviceAddress));
+  Serial.println();    
+}
+
+// main function to print information about a device
+void printData(DeviceAddress deviceAddress)
+{
+  Serial.print("Device Address: ");
+  printAddress(deviceAddress);
+  Serial.print(" ");
+  printTemperature(deviceAddress);
+  Serial.println();
+}
+
