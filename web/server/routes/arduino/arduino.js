420785401aa49f1bd3f7d33be531c07f382c88be
Motti Horesh <mottihoresh@gmail.com>
2014-06-10 22:46:58 -0400

    trying to get arduino comm to work

--- web/server/routes/arduino/arduino.js
+++ web/server/routes/arduino/arduino.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     var serialPort = app.serialPort;
     var writeAndDrain = app.writeAndDrain;
@@ -10,40 +10,39 @@ module.exports = function(app) {
 //    console.log('need to include some controllers in here...');
 
 
-    app.get('/arduino/test',function(req,res){
+    app.get('/arduino/test', function (req, res) {
         writeAndDrain('C00\n');
     });
 
 
+    serialPort.on('open', function (err) {
+        console.log('Serial Connection to Arduino has been established');
+        writeAndDrain('C00\n');
 
-        serialPort.on('open',function(err){
-            console.log('Serial Connection to Arduino has been established');
-            writeAndDrain('C00\n');
-
-        })
-            .on('data',function(data){
-                console.log('From arduino router:',data);
-            });
-
+    })
+        .on('data', function (data) {
+//            console.log('From arduino router:', data);
+            console.log('received some stuff from arduino', data);
+        });
 
 
     app.route('/api/v1/arduino')
-        .get(function(req, res){
+        .get(function (req, res) {
             console.log('GET', req.body);
 //            app.writeAndDrain('tty.linvor-DevB');
             app.writeAndDrain('C00\n');
             res.jsonp({'done': 'ok'});
 
         })
-        .put(function(req, res){
+        .put(function (req, res) {
 //            console.log('PUT', req.body);
             res.jsonp({'done': 'ok'});
         })
-        .delete(function(req, res){
+        .delete(function (req, res) {
 //            console.log('DELETE', req.body);
             res.jsonp({'done': 'ok'});
         })
-        .post(function(req, res){
+        .post(function (req, res) {
 //            console.log('POST', req.body);
             res.jsonp({'done': 'ok'});
         });
