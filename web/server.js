7b37cfc00380fb37d1e6fd26d423a3a3ae2970c6
Motti Horesh <mottihoresh@gmail.com>
2014-07-01 15:47:41 -0400

    removed console log from lights (used for debug)

--- web/server.js
+++ web/server.js
@@ -114,7 +114,6 @@ app.arduino.verifyCommand = function(commandObj) {
 
 
             case '04':  // lights
-                console.log(commandObj);
                 console.log(timeStamp+': Updating light channel '+ commandObj.pwmchannel + ' on chip '+commandObj.pwmchip+', new value is: '+commandObj.intensity);
 
                 break;
